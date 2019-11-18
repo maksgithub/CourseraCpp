78a90b6bccd11e97c6d76d508f3c3325a3990078
Maksym Tatsenko <maksym.tasenko@skylum.com>
2019-11-18 08:26:48 +0200

    add

--- N1/Двоичные числа/Двоичные числа.cpp
+++ N1/Двоичные числа/Двоичные числа.cpp
@@ -1,32 +1,11 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Двоичные числа.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <string>
 #include <iostream>
 
 int main()
 {
-    std::string s;
-    std::cin >> s;
-    if (s.length() == 0)
-        return 1;
-    auto a = s.find("f");
-    if (a > s.length())
-        std::cout << -2;
-        return -1;
-    s.replace(a, 1, "3");
-    auto b = s.find("f");
-    if (b > 0 && b <= s.length())
-        std::cout << b;
-    else if(a >= 0 && a <= s.length())
-    {
-        std::cout << -1;
-    }
-    else
-    {
-        std::cout << -2;
-    }
-    return 1;
+    std::cout << "Hello World!\n"; 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
