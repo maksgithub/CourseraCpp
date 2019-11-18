26e55987cb859338ff08be66e4ef02cfc5dfebd8
Maksym Tatsenko <maksym.tasenko@skylum.com>
2019-11-18 07:43:52 +0200

    Add polindrm

--- Polindrom/Source.cpp
+++ Polindrom/Source.cpp
@@ -7,24 +7,37 @@ using namespace std;
 bool IsPalindrom(string s)
 {
     int i = 0;
-    auto last = s.length() - 1;
+    auto length = s.length();
+    auto last = length - 1;
     bool res = i <= last;
-    while (i < last)
+    while (i < last && length > 0)
     {
         if (s[i] != s[last])
         {
             res = false;
             break;
         }
-        i++;
-        last -= i;
+        ++i;
+        last -= 1;
+    }
+    return res;
+}
+
+vector<string> PalindromFilter(vector<string> words, int minLength)
+{
+    vector<string> res;
+    for (auto p : words)
+    {
+        if (IsPalindrom(p) && p.length() >= minLength)
+        {
+            res.push_back(p);
+        }
     }
     return res;
 }
 
 int main()
 {
-    string s;
-    std::cin >> s;
-    cout << IsPalindrom(s);
+    vector<string> res = { "weew", "bro", "code" };
+    auto r = PalindromFilter(res, 4);
 }
