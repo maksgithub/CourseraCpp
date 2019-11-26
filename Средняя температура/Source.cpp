b1504d3cfe76bbf5546385ec22fee1aafbd36ce5
Maksym Tatsenko <maksym.tasenko@skylum.com>
2019-11-26 16:03:40 +0200

    Queue

--- Средняя температура/Source.cpp
+++ Средняя температура/Source.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+struct Man
+{
+    bool Status;
+};
 
 int main()
 {
-    int n, sum = 0;
+    int n;
     std::cin >> n;
 
-    std::vector<int> vector(n);
-    std::vector<int> vector2;
+    std::vector<Man> vector;
 
     for (size_t i = 0; i < n; i++)
     {
-        int a;
+        int b;
+        std::string a;
         std::cin >> a;
-        sum += a;
-        vector[i] = a;
-    }
-
-    for (size_t i = 0; i < n; i++)
-    {
-        if(vector[i] > sum/n)
+        if (a == "COME")
         {
-            vector2.push_back(i);
+            std::cin >> b;
+            if (b > 0)
+            {
+                for (int i = 0; i < b; ++i)
+                {
+                    Man m{};
+                    m.Status = true;
+                    vector.push_back(m);
+                }
+            }
+            else if (b < 0)
+            {
+                for (int i = 0; i < abs(b); ++i)
+                {
+                    vector.pop_back();
+                }
+            }
         }
-    }
-    std::cout << vector2.size() << "\n";
 
-    for (int vector21 : vector2)
-    {
-        std::cout << vector21 << " ";
+        if (a == "QUIET")
+        {
+            std::cin >> b;
+            vector[b].Status = true;
+        }
+
+        if (a == "WORRY")
+        {
+            std::cin >> b;
+            vector[b].Status = false;
+        }
+
+        if (a == "WORRY_COUNT")
+        {
+            int num_items3 = std::count_if(vector.begin(), vector.end(), [](Man i) {return !i.Status; });
+            std::cout << num_items3 << std::endl;
+        }
     }
 }
